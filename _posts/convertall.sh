ca41322d70721e17dede1a76b5746b65cdc8549b
Alistair Miles <alimanfoo@googlemail.com>
2016-04-15 00:34:35 +0100

    add hdf5 post

--- _posts/convertall.sh
+++ _posts/convertall.sh
@@ -3,3 +3,4 @@
 nbconvert.sh 2015-09-15-introducing-scikit-allel 'Introducing scikit-allel'
 nbconvert.sh 2015-09-21-estimating-fst 'Estimating F<sub>ST</sub>'
 nbconvert.sh 2015-09-28-fast-pca 'Fast PCA'
+nbconvert.sh 2016-04-14-to-hdf5-and-beyond "To HDF5 and beyond"
