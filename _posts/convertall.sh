fb8934057148735a13d2e4ef3d4a946dbb67c7a9
Alistair Miles <alimanfoo@googlemail.com>
2018-04-12 11:01:21 +0100

    update reading vcf tutorial

--- _posts/convertall.sh
+++ _posts/convertall.sh
@@ -14,6 +14,7 @@ nbconvert.sh 2016-10-04-plotting-african-ecosystems "Plotting African ecosystems
 nbconvert.sh 2017-01-23-go-faster-python "Go faster Python"
 nbconvert.sh 2017-01-25-vgsc-gene-models "VGSC gene models"
 nbconvert.sh 2017-02-14-mendelian-transmission "Mendelian transmission"
+nbconvert.sh 2017-06-14-read-vcf "Extracting data from VCF files"
 nbconvert.sh 2018-04-09-selecting-variants "Selecting variants"
 
 
