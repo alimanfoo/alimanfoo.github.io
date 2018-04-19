8b69545f9e761b74090134d794e34efc5a42f38f
Alistair Miles <alimanfoo@googlemail.com>
2018-04-19 12:54:20 +0100

    add selecting samples example

--- _posts/convertall.sh
+++ _posts/convertall.sh
@@ -15,7 +15,4 @@ nbconvert.sh 2017-01-23-go-faster-python "Go faster Python"
 nbconvert.sh 2017-01-25-vgsc-gene-models "VGSC gene models"
 nbconvert.sh 2017-02-14-mendelian-transmission "Mendelian transmission"
 nbconvert.sh 2017-06-14-read-vcf "Extracting data from VCF files"
-nbconvert.sh 2018-04-09-selecting-variants "Selecting variants"
-
-
-
+nbconvert.sh 2018-04-09-selecting-variants "Selecting variants and samples"
