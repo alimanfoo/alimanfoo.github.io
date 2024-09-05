183a7aec07ccd8e4109979868c36a38aa540ba1d
Alistair Miles <am23@sanger.ac.uk>
2024-09-05 12:17:27 +0000

    wip

---
+++ src/2024-09-05-kenya-coluzzii-njt/convert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while inotifywait -e close_write post.ipynb
+do
+    pixi run jupyter nbconvert post.ipynb \
+	 --to html \
+	 --template basic \
+	 --no-prompt \
+	 --TagRemovePreprocessor.enabled=True \
+	 --TagRemovePreprocessor.remove_input_tags remove_input \
+	 --TagRemovePreprocessor.remove_all_outputs_tags remove_output \
+	 --TagRemovePreprocessor.remove_cell_tags remove_cell
+done
