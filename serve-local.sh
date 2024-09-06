0232cb0558aa68af20a6487e6e3bc79f10618a10
Alistair Miles <alimanfoo@googlemail.com>
2024-09-06 18:04:21 +0100

    landing page

--- serve-local.sh
+++ serve-local.sh
@@ -5,4 +5,4 @@ set -xeuo pipefail
 # https://github.com/guard/listen/blob/master/README.md#adapter-warnings
 export LISTEN_GEM_ADAPTER_WARN_BEHAVIOR=silent
 
-bundle exec jekyll serve --incremental
+bundle exec jekyll serve
