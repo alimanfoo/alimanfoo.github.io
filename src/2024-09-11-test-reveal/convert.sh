708e6e94adccbcccb83eb798e8fe982d6a5c4aba
Alistair Miles <alimanfoo@googlemail.com>
2024-09-11 10:51:31 +0100

    test reveal

---
+++ src/2024-09-11-test-reveal/convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while inotifywait -e close_write slides.ipynb
+do
+    pixi run jupyter nbconvert slides.ipynb \
+	 --to slides \
+	 --no-prompt \
+	 --TagRemovePreprocessor.enabled=True \
+	 --TagRemovePreprocessor.remove_input_tags remove_input \
+	 --TagRemovePreprocessor.remove_all_outputs_tags remove_output \
+	 --TagRemovePreprocessor.remove_cell_tags remove_cell
+done
