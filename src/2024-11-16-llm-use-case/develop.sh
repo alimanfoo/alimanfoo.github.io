6f2212bc594a49d27efaa112bb759316abf7f888
Alistair Miles <alimanfoo@googlemail.com>
2024-11-16 16:15:09 -0600

    wip

---
+++ src/2024-11-16-llm-use-case/develop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ../../ && bundle exec jekyll serve &
+
+pixi run jupyter lab &
+
+while inotifywait -e close_write post.ipynb
+do
+    pixi run jupyter nbconvert post.ipynb \
+	 --to html \
+	 --template basic \
+	 --no-prompt \
+	 --TagRemovePreprocessor.enabled=True \
+	 --TagRemovePreprocessor.remove_input_tags remove_input \
+	 --TagRemovePreprocessor.remove_all_outputs_tags remove_output \
+	 --TagRemovePreprocessor.remove_cell_tags remove_cell
+done
